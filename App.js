785cc6bc1a8bfdfd6adfc885b8b50776d9eaf1ff
MaksimKmet <kmetmaksim686@gmail.com>
2023-01-16 09:04:10 +0100

    last erro is  with tab(query) and finish

--- App.js
+++ App.js
@@ -3,13 +3,31 @@ let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById('input-btn')
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabs = []
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
 } 
+
+// we have poblem with this one cant read "query pro"
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({
+        active: true, curentWindow: true
+    }, (tabs) => {
+            myLeads.push(tabs[0].url)
+            localStorage.setItem("myLeads", JSON.stringify(myLeads))
+            return render(myLeads)
+    } )
+    
+    //console.log(tabs[0].url)
+   
+}
+)
+
 
 deleteBtn.addEventListener("dblclick", () => {
     localStorage.clear()
